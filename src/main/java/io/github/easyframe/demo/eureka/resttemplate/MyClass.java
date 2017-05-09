50a44fc7860deb89dcfff0678bac095b53c5c0e1
linzhaoming <teleyic@gmail.com>
2017-05-09 10:05:03 +0800

    Add Copyright

--- src/main/java/io/github/easyframe/demo/eureka/resttemplate/MyClass.java
+++ src/main/java/io/github/easyframe/demo/eureka/resttemplate/MyClass.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.easyframe.demo.eureka.resttemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +32,8 @@ public class MyClass {
     private RestTemplate loadBalanced;
 
     public String doOtherStuff() {
-        //url需要为虚拟的host name,如service name,并非实际host name
-        //Ribbon client用于创建全物理地址. 设置地方查看RibbonAutoConfiguration
+        //url need to be virtual host name, such as service name, not the real host name
+        //Ribbon client use it to create the full physical address, settings:RibbonAutoConfiguration
         return loadBalanced.getForObject("http://stores/stores", String.class);
     }
 
